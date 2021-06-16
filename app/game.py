000a267f8dbb75eb03f8b35be4ce0f9b46c1343d
psk264 <84349071+psk264@users.noreply.github.com>
2021-06-15 20:33:32 -0400

    refactored code

--- app/game.py
+++ app/game.py
@@ -1,4 +1,5 @@
 
+from _typeshed import NoneType
 from random import choice
 
 #
@@ -22,23 +23,43 @@ print("COMPUTER CHOICE:", c)
 # DETERMINATION OF WINNER
 #
 
-if u == "rock" and c == "rock":
-    print("It's a tie!")
-elif u == "rock" and c == "paper":
-    print("The computer wins")
-elif u == "rock" and c == "scissors":
-    print("The user wins")
+def determine_winner(u, c):
+    if u == "rock" and c == "rock":
+        # print("It's a tie!")
+        return None
+    elif u == "rock" and c == "paper":
+        # print("The computer wins")
+        return c
+    elif u == "rock" and c == "scissors":
+        # print("The user wins")
+        return u
 
-elif u == "paper" and c == "rock":
-    print("The computer wins")
-elif u == "paper" and c == "paper":
-    print("It's a tie!")
-elif u == "paper" and c == "scissors":
-    print("The user wins")
+    elif u == "paper" and c == "rock":
+        # print("The computer wins")
+        return c
+    elif u == "paper" and c == "paper":
+        # print("It's a tie!")
+        return None
+    elif u == "paper" and c == "scissors":
+        # print("The user wins")
+        return u
 
-elif u == "scissors" and c == "rock":
-    print("The computer wins")
-elif u == "scissors" and c == "paper":
-    print("The user wins")
-elif u == "scissors" and c == "scissors":
-    print("It's a tie!")
+    elif u == "scissors" and c == "rock":
+        # print("The computer wins")
+        return c
+    elif u == "scissors" and c == "paper":
+        # print("The user wins")
+        return u
+    elif u == "scissors" and c == "scissors":
+        # print("It's a tie!")
+        return None
+
+winner = determine_winner(u,c)
+
+    #Refactoring
+if winner == u:
+    print("YOU won")
+elif winner == c:
+    print("COMP won")
+else:
+    print("Tie")
